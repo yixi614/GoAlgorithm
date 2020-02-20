56b66bb830cf3135ee8e4162ade646e0274d6565
yixi <wuming-kun@163.com>
2020-02-20 23:27:32 +0800

    Delete a known node in a linked list without knowing the head

--- src/main/java/Common/LinkedNode.java
+++ src/main/java/Common/LinkedNode.java
@@ -11,6 +11,14 @@ public class LinkedNode {
     this.next = next;
   }
 
+  // Delete this node from a list
+  public void delete() {
+    if(this != null && this.next != null) {
+      this.val = this.next.val;
+      this.next = this.next.next;
+    }
+  }
+
   public static String printList(LinkedNode head) {
     StringBuilder s = new StringBuilder();
     LinkedNode p = head;
@@ -19,6 +27,14 @@ public class LinkedNode {
       p = p.next;
     }
     return s.toString();
+  }
+
+  public static void main(String[] args) {
+    int[] a = {1,2,3,4,5,6};
+    LinkedNode head = CommonBuilder.buildLindedList(a);
+    LinkedNode two = head.next;
+    two.delete();
+    System.out.println(head.printList(head));
   }
 
 }
