801f0788feb749441ec71a4adeec12efb5a4c185
yixi <wuming-kun@163.com>
2021-05-28 18:20:48 +0800

    test

--- src/main/java/LinkedList/LRU.java
+++ src/main/java/LinkedList/LRU.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *The task is to design and implement methods of an LRU cache.
+ * The task is to design and implement methods of an LRU cache.
  * The class has two methods get and set which are defined as follows.
  * get(x)   : Gets the value of the key x if the key exists in the cache otherwise returns -1
  * set(x,y) : inserts the value if the key x is not already present. If the cache reaches its capacity it should invalidate the least recently used item before inserting the new item.
